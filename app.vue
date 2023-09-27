5105bf4fb0bbb9681399532882bed6c0456434f4
ali_jarkani <alijarkani@gmail.com>
2023-09-28 02:24:52 +0330

    Initial set of files were added, vuetify and i18n modules were installed and configured

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { isRtl } = useRtl()
+</script>
+
+<template>
+  <v-locale-provider :rtl="isRtl">
+    <NuxtPage />
+  </v-locale-provider>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "vuetify-nuxt-module",
+    "@nuxtjs/i18n"
+  ],
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        file: 'en.json',
+        dir: 'ltr',
+        name: 'English',
+        flag: '🇺🇸',
+      },
+      {
+        code: 'fa',
+        file: 'fa.json',
+        dir: 'rtl',
+        name: 'فارسی',
+        flag: '🇮🇷',
+      },
+    ],
+    langDir: 'translations/',
+    defaultLocale: 'en',
+  }
+})
