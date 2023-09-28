0b098846b00558fbc0987b332ac6a6788da4f8b6
ali_jarkani <alijarkani@gmail.com>
2023-09-28 21:45:32 +0330

    A background image was added to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,12 +9,42 @@ const display = useDisplay()
   <v-main>
     <NavigationDrawer />
     <AppBar v-if="display.height.value && display.mobile.value" />
-    <slot />
+    <div class="abstract-bg pt-2 pt-lg-10 px-lg-4">
+      <div class="content">
+        <slot />
+      </div>
+    </div>
   </v-main>
 </template>
 
 <style>
 .v-main {
     overflow: hidden;
+}
+
+.abstract-bg {
+    position: relative;
+    min-height: calc(100vh - 60px);
+}
+
+.abstract-bg:after {
+    content: ' ';
+    display: block;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0.05;
+    background-position-x: center;
+    background-position-y: bottom;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-image: url("/bg-01.png");
+}
+
+.content {
+  position: relative;
+  z-index: 1;
 }
 </style>
