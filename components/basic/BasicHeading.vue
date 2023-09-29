2338c3d37fc82ff68be5c7ae4d3286b389c2aa3c
ali_jarkani <alijarkani@gmail.com>
2023-09-29 21:00:33 +0330

    Basic heading and Paragraph were added

---
+++ components/basic/BasicHeading.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2 class="text-primary mb-4 text-h3 font-italic font-weight-bold">
+    <slot />
+  </h2>
+</template>
---
+++ components/basic/BasicParagraph.vue
@@ -0,0 +1,5 @@
+<template>
+  <p class="mb-4 text-justify big-text">
+    <slot />
+  </p>
+</template>
