89a4526e7155794c640c373ae10ad255469ba2b1
ali_jarkani <alijarkani@gmail.com>
2023-09-29 21:05:31 +0330

    Skill page was designed

--- components/usecase/SkillsGroup.vue
+++ components/usecase/SkillsGroup.vue
@@ -3,7 +3,6 @@ import SkillsList from '~/components/usecase/SkillsList.vue'
 import { SkillGroupItem } from '~/types/skill'
 
 interface Props {
-    title: String
     groups: Array<SkillGroupItem>
     template: String
 }
@@ -11,14 +10,11 @@ interface Props {
 const display = useDisplay()
 
 const props = defineProps<Props>()
-const { title, groups, template } = props
+const { groups, template } = props
 </script>
 
 <template>
   <div>
-    <h4 class="text-h4 text-primary font-weight-bold font-italic mb-8">
-      {{ $t(`skill.${title}`) }}
-    </h4>
     <div class="ps-0 ps-md-10 d-flex flex-column" :class="{'grid-view': !display.mobile.value}" :style="{gridTemplateAreas: template}">
       <SkillsList
         v-for="group in groups"
---
+++ pages/skills.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import skills from '~/constants/skills.json'
+import SkillsGroup from '~/components/usecase/SkillsGroup.vue'
+
+const blocks = [
+  'backend frontend',
+  'scientific frontend',
+  'devops frontend',
+  'ai_libraries vcs',
+  'ai_libraries database',
+]
+
+const template = ref(blocks.map(e => `'${e}'`).join(' '))
+</script>
+
+<template>
+  <v-container>
+    <h4 class="text-h4 text-primary font-weight-bold font-italic mb-8">
+      {{ $t('sidebar.skills') }}
+    </h4>
+    <skills-group :template="template" :groups="skills" />
+    <basic-paragraph class="mt-10 mb-16 font-italic">
+      <v-icon>mdi-dots-horizontal-circle-outline</v-icon>
+      {{ $t('skill.more') }}
+    </basic-paragraph>
+  </v-container>
+</template>
